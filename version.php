e55d583ce47a848207cba20edbdad49737dc3284
ponlawat-w <ponlawat_w@outlook.co.th>
2025-09-26 11:57:06 +0700

    Bumped version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_customgroups';
-$plugin->release = '0.0.0';
-$plugin->version = 2023032900;
+$plugin->release = '0.1.0';
+$plugin->version = 2025092600;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_ALPHA;
